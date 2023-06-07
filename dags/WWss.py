afcb1b7a2268ed8f456862348effd87d9e841f62
I Dewa Gede Raka Murdiarta <dewamurdiarta@gmail.com>
2023-06-07 15:18:36 +0800

    test

--- dags/WWss.py
+++ dags/WWss.py
@@ -8,11 +8,23 @@ from astro import sql as aql
 import pandas as pd
 import pendulum
 
-import pandas as pd
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 
 @aql.dataframe(task_id="task")
 def task_func():
-    print('hello')
+    def print_hello():
+        return 'Hello world from first Airflow DAG!'
+    
+    dag = DAG('hello_world', description='Hello World DAG',
+              schedule_interval='0 12 * * *',
+              start_date=datetime(2017, 3, 20), catchup=False)
+    
+    hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+    
+    hello_operator
 
 @dag(
     schedule="0 0 * * *",
